f7a4ec298707be52dbed68474de9d5f909ce3fc7
innerclutch33 <risebold9@gmail.com>
2026-03-23 16:26:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openInstagram() {
+  window.open("https://www.instagram.com/msquare_05/", "_blank");
+}
+
+function openWhatsApp() {
+  window.open("https://wa.me/919XXXXXXXXX", "_blank");
+}
+
+function contactNow() {
+  openWhatsApp();
+}
